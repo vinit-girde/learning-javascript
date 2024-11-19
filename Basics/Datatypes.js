75171b343c245992fbdc999dc407aa0e6d0c0e70
vinit-girde <vinitgirde@gmail.com>
2024-11-19 19:42:16 +0530

    datatype

---
+++ Basics/Datatypes.js
@@ -0,0 +1,18 @@
+// Primitive Datatype
+let name = "Alan";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
