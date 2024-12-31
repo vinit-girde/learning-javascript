6de9d7f2557ffff36c627be0d896049e74ba35b3
vinit-girde <vinitgirde@gmail.com>
2025-01-01 00:43:25 +0530

    Some more methods of array

--- Basic/Arrays.js
+++ Basic/Arrays.js
@@ -67,6 +67,42 @@ let joinedArr = arrEx.join();
 // console.log("SPliced Arr", splicedArr); // SPliced Arr [ 10, 20, 30 ]
 // console.log("Original Arr after splice() operation", originalArr); // Original Arr after splice() operation [40,50,60,70,80,90,100]
 
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// concat() --> Combines the array into new array
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+// flat() --> This methods converts all elements concatenated into single array.
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array); // [1,2,3,4,5,6,7,6,7,4,5]
+
+// isArray() --> Checks if the passed values is an array.
+console.log(Array.isArray("Vinit")); //false
+
+// from() --> Creates copy of passed value into an array.
+console.log(Array.from("Vinit")); // [ 'V', 'i', 'n', 'i', 't' ]
+console.log(Array.from({ name: "Vinit" })); // []  becoz the object contains key value pair in it.
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+// of() --> creates a new array from passed arguments
+console.log(Array.of(score1, score2, score3));
+
 /* 
     Note: 
     //  When we console the value at push or pop it gives length. ex at line no 10.
