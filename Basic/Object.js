402a1d106bb3c7f384af03188b56f508e927a422
vinit-girde <vinitgirde@gmail.com>
2025-01-03 11:16:09 +0530

    objects

--- Basic/Object.js
+++ Basic/Object.js
@@ -1,30 +1,37 @@
-// singleton
-// Object.create
+// Objects in js can be created using 2 ways: Object.create(),let obj={}.
+// Objects  consists of key value pair.
+// singleton --> Object are only singleton when created using construtor.
+// Object.create is used in creating object using constructor.
 
-// object literals
+// object literals i.e -> obj={}
 
+// How to use Symbol datatype in object and access that property.
 const mySym = Symbol("key1");
 
 const JsUser = {
   name: "vinit",
-  "full name": "vinit ",
+  "full name": "vinit girde",
   [mySym]: "mykey1",
+  age: 19,
   age: 18,
-  location: "Jaipur",
+  location: "Nagpur",
   email: "vinit@google.com",
   isLoggedIn: false,
   lastLoginDays: ["Monday", "Saturday"],
 };
+console.log("One", JsUser);
+// console.log(JsUser.email) // vinit@google.com
+// console.log(JsUser["email"]) // vinit@google.com
+// console.log(JsUser["full name"]) // vinit girde
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// To access symbol datatype value use [] with key name without string.
+// console.log(JsUser[mySym]) // mykey1
 
 JsUser.email = "vinit@chatgpt.com";
-// Object.freeze(JsUser)
+console.log("Two", JsUser);
+// Object.freeze(JsUser);
 JsUser.email = "vinit@microsoft.com";
-// console.log(JsUser);
+// console.log("After Freeze", JsUser);
 
 JsUser.greeting = function () {
   console.log("Hello JS user");
@@ -33,5 +40,5 @@ JsUser.greetingTwo = function () {
   console.log(`Hello JS user, ${this.name}`);
 };
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+JsUser.greeting(); // Hello JS user
+JsUser.greetingTwo(); // Hello JS user vinit
