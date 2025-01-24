6755d80c231da246cbf19937916918be5aee2a80
vinit-girde <vinitgirde@gmail.com>
2025-01-24 23:53:03 +0530

    random number guess project start

---
+++ DOM/DOM-Projects/Project-04-GuessTheNumber/script.js
@@ -0,0 +1,7 @@
+let randomNumber;
+// Function to get random number betweeen 1 to 100
+function getRandomNumber() {
+  randomNumber = parseInt(Math.random() * 100 + 1);
+}
+// calling function to return random number
+getRandomNumber();
