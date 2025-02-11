32e21a484308be76db249086aa2c390cda27d8df
vinit-girde <vinitgirde@gmail.com>
2025-02-12 01:57:08 +0530

    closure example

--- Advance/Closure/closure.js
+++ Advance/Closure/closure.js
@@ -4,6 +4,22 @@
                 Whenever function is returned, even if its vanished in execution context but still it remembers the reference it was pointing to. Its not just that function alone it returns but the entire closure and that's where it becomes interesting .
 
                 A closure in JavaScript is a function that remembers the variables from its outer scope even after the outer function has finished executing.
+
+                // ------------------------------------------------
+                
+                An important topic in JavaScript is the concept of closures. When a function is created, it has access to a reference to all the variables declared around it, also known as it's lexical environment. The combination of the function and its enviroment is called a closure. This is a powerful and often used feature of the language.
+
+                function createAdder(a) {
+                    function f(b) {
+                        const sum = a + b;
+                        return sum;
+                    }
+                    return f;
+                }
+                const f = createAdder(3);
+                console.log(f(4)); // 7
+
+                In this example, createAdder passes the first parameter a and the inner function has access to it. This way, createAdder serves as a factory of new functions, with each returned function having different behavior.
 */
 
 function x() {
